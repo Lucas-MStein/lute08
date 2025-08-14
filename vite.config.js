01fd27e40503aee53cabd03cd5274b82da6e75ce
Lucas-Maurice Stein <lms@koempf24.de>
2025-08-14 23:49:14 +0200

    deploment fix for lute

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // 🔁 Hier den Repo-Namen eintragen
 export default defineConfig({
     plugins: [react()],
-    base: '/freshprinceofberlin/', // z. B. dein GitHub-Repo-Name
+    base: '/', // z. B. dein GitHub-Repo-Name
 });
