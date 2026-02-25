13f666186d263144dcfe6b91917a3566bbbfd9ad
Lucas-Maurice Stein <lms@koempf24.de>
2026-02-25 12:08:18 +0100

    cleaned up package.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// 🔁 Hier den Repo-Namen eintragen
 export default defineConfig({
     plugins: [react()],
-    base: '/', // z. B. dein GitHub-Repo-Name
-});
+})
